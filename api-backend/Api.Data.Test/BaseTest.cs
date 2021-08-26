35a4f263db260358117a5d7cef5787fe4f63ab41
Danielle Aparecida Baer dos Santos <danielle.baer@ambevtech.com.br>
2021-08-26 16:19:01 -0300

    implementando teste com a biblioteca faker

---
+++ api-backend/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,46 @@
+using System;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseMySql($"Persist Security Info=True;Server=localhost;Database={dataBaseName};User=root;Password=mysql@123"),
+                ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
---
+++ api-backend/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -0,0 +1,41 @@
+using System;
+using Api.Data.Context;
+using Api.Data.Implementations;
+using Api.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace Api.Data.Test
+{
+    public class UsuarioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UsuarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "CRUD de Usuário")]
+        [Trait("CRUD", "UserEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Completo()
+        {
+            using (var context = _serviceProvider.GetService<MyContext>())
+            {
+                UserImplementation _repositorio = new UserImplementation(context);
+                UserEntity _entity = new UserEntity()
+                {
+                    Email = Faker.Internet.Email(),
+                    Name = Faker.Name.FullName()
+                };
+
+                var _registroCriado = await _repositorio.InsertAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal(_entity.Email, _registroCriado.Email);
+                Assert.Equal(_entity.Name, _registroCriado.Name);
+                Assert.False(_registroCriado.Id == Guid.Empty);
+            }
+        }
+    }
+}
